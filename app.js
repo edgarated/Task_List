2c2422234881e30e11e500092f8d02739ce5e07f
edgarated <[chukwuebukaaneke22@gmail.com]>
2019-04-10 20:12:48 +0100

    added filter function

--- app.js
+++ app.js
@@ -81,10 +81,24 @@ TaskList.removeChild(TaskList.firstChild);
 
  //filter events
 
- function filterTasks(){
-
+ function filterTasks(e){
+  const text = e.target.value.toLowerCase()
+  document.querySelectorAll(".collection-item").forEach
+  (function(task){
+  const item = task.firstChild.textContent;
+  if(item.toLowerCase().indexOf(text) != -1){
   
- }
+  task.style.display = "block";
+  } else{
+  
+    task.style.display = "none";
+  }
+  
+  
+  })
+  
+   }
+  
 
 
 
